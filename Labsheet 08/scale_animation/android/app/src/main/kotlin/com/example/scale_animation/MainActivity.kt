02869cb552b1cf4207fc72dc9b0cd2a9e53e34d4
BANDARAKDT <deshanibandarawas@gmail.com>
2025-03-27 09:56:04 +0530

    2025.03.27 Scale animation

---
+++ Labsheet 08/scale_animation/android/app/src/main/kotlin/com/example/scale_animation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.scale_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Labsheet 08/scale_animation/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Animation Example'),
+        ),
+        body: ScaleAnimation(),
+      ),
+    );
+  }
+}
+
+class ScaleAnimation extends StatefulWidget {
+  @override
+  _ScaleAnimationState createState() => _ScaleAnimationState();
+}
+
+class _ScaleAnimationState extends State<ScaleAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller; // Use 'late' keyword
+  late Animation<double> _animation; // Use 'late' keyword
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ScaleTransition(
+        scale: _animation,
+        child: Container(
+          width: 200.0,
+          height: 200.0,
+          color: Colors.green,
+          child: Center(
+            child: Text(
+              'Scale',
+              style: TextStyle(color: Colors.white, fontSize: 24.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
